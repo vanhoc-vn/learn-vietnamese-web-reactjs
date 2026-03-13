3daa840be2e88ee52700ff02ed828356de9059f7
vanhoc-vn <hoclv.04@gmail.com>
2026-03-13 16:11:17 +0700

    2.21

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,33 +9,42 @@ class MyComponent extends React.Component { // class đã trả thành 1 class c
         age: 30
     };
 
-    handleClick (event) {
+    handleClick(event) {
         console.log('Click me')
-        console.log("Randum", Math.floor((Math.random() * 100)  +1))
+        console.log("Randum", Math.floor((Math.random() * 100) + 1))
         this.setState({
-            name : 'Van Hoc 2.0',
-            age : Math.floor((Math.random() * 100)  +1),
+            name: 'Van Hoc 2.0',
+            age: Math.floor((Math.random() * 100) + 1),
         })
         this.setState({
-            age : Math.floor((Math.random() * 100)  +1),
+            age: Math.floor((Math.random() * 100) + 1),
         })
     }
 
     handleOnMoverOver(event) {
         // console.log(event.pageX)
     }
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handleClickSubmit = (event) => {
+        event.preventDefault(); // ngan chan reload trang
+        alert('Me')
+        console.log(this.state)
+    }
     // JSX cho phép viết code js trong html
     render() {
         return (
             <div>
-
-                My name is {this.state.name} and I'm from {this.state.address}
-                <button onMouseOver={this.handleOnMoverOver}>
-                    Hover me
-                </button>
-                <button onClick={(event) => {this.handleClick(event)}}>
-                    Click me
-                </button>
+                My name is {this.state.name} and I'm from {this.state.age}
+                <form onSubmit={(event) => this.handleClickSubmit(event)}>
+                    <input type="text"
+                        onChange={(event) => this.handleOnChangeInput(event)}
+                    />
+                    <button> Submit </button>
+                </form>
             </div>
         );
     }
