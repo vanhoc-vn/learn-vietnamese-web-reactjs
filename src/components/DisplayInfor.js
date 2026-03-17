594c68e3bee0ea581614679e87a0009f99ce3cdb
vanhoc-vn <hoclv.04@gmail.com>
2026-03-17 16:17:30 +0700

    2.25

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,14 +1,45 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
     render() {
-       // destructuring array/object
-        const {name, age} = this.props
+        // destructuring array/object
+        const { listUsers } = this.props
         // props = properties: Tài sản, vì nó dùng để truyền dữ liệu từ cha sang cho con.
         return (
             <div>
-                <div> My name's {name}</div>
-                <div> My Age is {age}</div>
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                        {
+                            this.state.isShowListUser === true ? "Hide list User" : "Show list User"
+                        }
+                    </span>
+                </div>
+                {
+                    this.state.isShowListUser &&
+
+                    <div>
+                        {
+                            listUsers.map((user, index) => {
+                                return (
+                                    <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                        <div> My name's {user.name}</div>
+                                        <div> My Age is {user.age}</div>
+                                        <hr />
+                                    </div>
+
+                                )
+                            })}
+                    </div>
+                }
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,15 +5,25 @@ import UserInfor from './UserInfor';
 import DisplayInfor from './DisplayInfor';
 class MyComponent extends React.Component { // class đã trả thành 1 class components
     // JSX cho phép viết code js trong html
+
+    state = {
+        listUsers: [
+            { id: 1, name: "Van Hoc", age: "16" },
+            { id: 2, name: "Van Duc", age: "22" },
+            { id: 3, name: "Van Hoa", age: "69" },
+
+        ]
+    }
     render() {
-        const myInfor = ['ab', 'c', 'c']
+        // DRY; don't repeat yourself.
         return (
             <div>
                 <UserInfor />
                 <br /> <br />
-                <DisplayInfor name= "Van Hoc" age = "22" />
-                <hr/>
-                <DisplayInfor name= {"Van Hoa"} age = {26} myInfor={myInfor} />
+                <DisplayInfor
+                    listUsers={this.state.listUsers}
+
+                />
             </div>
         );
     }
