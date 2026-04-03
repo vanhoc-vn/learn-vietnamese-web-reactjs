e1f42f6931de6376424c4558037c683b71f85aa0
vanhoc-vn <hoclv.04@gmail.com>
2026-04-03 15:36:52 +0700

    3.39

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,40 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
-// stateless (k co state) vs stateful (co kiem soat data nen dung class)
-// class DisplayInfor extends React.Component {
-//     render() {
-//         console.log('Call me render ')
-//         // destructuring array/object
-//         const { listUsers } = this.props
-//         // props = properties: Tài sản, vì nó dùng để truyền dữ liệu từ cha sang cho con.
-//         return (
-//             <div className='display-infor-container'>
-//                 {
-//                     true &&
-//                     <>
-//                         {
-//                             listUsers.map((user, index) => {
-//                                 return (
-//                                     <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                         <div>
-//                                             <div> My name's {user.name}</div>
-//                                             <div> My Age is {user.age}</div>
-//                                         </div>
-//                                         <div>
-//                                             <button onClick={() => this.props.handleDeleteUser(user.id)}> Delete </button>
-//                                         </div>
-//                                     </div>
-
-//                                 )
-//                             })}
-//                     </>
-//                 }
-//             </div>
-//         )
-//     }
-// }
 const DisplayInfor = (props) => {
     const { listUsers } = props; // Ojb
 
@@ -46,7 +13,15 @@ const DisplayInfor = (props) => {
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser);
     }
+    console.log("Call me")
 
+    useEffect(() => { // Hàm render sẽ chạy trước sau đó sẽ chạy hàm này
+        if(listUsers.length === 0) {
+            alert( "Bạn đã xóa hết Users")
+        }
+        console.log("Hello useEffect")
+    }, [listUsers]
+    );
 
     return (
         <div className='display-infor-container'>
