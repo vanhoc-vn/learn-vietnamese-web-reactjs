7c73813483fe8314eff77c6a4adffac8cd4544de
vanhoc-vn <hoclv.04@gmail.com>
2026-04-06 15:10:33 +0700

    5.47

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
+import Header from './components/Header/Header';
+const App = () => {
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-container'>
-        Helo World &amp;
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
+  return (
+    <div className="app-container">
+      <Header/>
+    </div>
+  );
 }
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Helo World...Van Hoc
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
